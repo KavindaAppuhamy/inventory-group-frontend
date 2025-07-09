0495507ffda832afb47d466de6e793a83f4de6e7
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-07-09 19:20:25 +0530

    set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+import AdminDashboard from './pages/admin/adminDashboard';
+import EmployeeCreate from './pages/adminDashboardPages/employeeCreate';
+import ManageEmployee from './pages/adminDashboardPages/manageEmployee';
 
 function App() {
 
   return (
-    <>
-      <h1>Inventory System</h1>
-    </>
-  )
+    <Router>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<AdminDashboard />}>
+          <Route index element={<div>Welcome to Employee Management</div>} />
+          <Route path="employee-manage" element={<ManageEmployee />} />
+          <Route path="employee-create" element={<EmployeeCreate />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
